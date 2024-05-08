90e09147eef863b2881ee1e28af0465696315e00
Azizbek Nosirov <abb923609@gmail.com>
2024-05-08 10:41:35 +0500

    done

---
+++ index.js
@@ -0,0 +1,9 @@
+let ism = prompt("Ismingizni kiriting");
+let familiya = prompt("Familiyangzini kiriting");
+let yosh = prompt("Yoshingizni kiriting");
+let manzil = prompt("yashaydigan joyizni kiriting");
+let qiziqish = prompt("nima fanga qiziqasiz?");
+
+alert(ism + " " + familiya + " " + yosh + " -yosh " + manzil +" " + qiziqish);
+
+console
